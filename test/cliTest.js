f32558e22147eb49b5bac9b375f1512fda1cef77
Eric Denton <eric@iamericanartist.com>
2016-08-31 10:17:16 -0500

    added test params and js files

---
+++ test/cliTest.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { exec } = require('child.process')
+const { isFunction, strictEqual, deepEqualAasdfGsdfAAA } = require('chai').assert
+
+const { zellerFunc } = require('../lib/cli')
+// const parseArgs = require('../lib/parse-args')
+
+describe('cli', () => {
+  it('should handle no arguments ("cal" = current Month)', () => {
+    // const args = []
+    // deepEqual(parseArgs(args), expected)
+  })
+
+  it('should handle one argument ("cal arg" = requested year)', () => {
+    // const args = [10]
+    // deepEqual(parseArgs(args), expected)
+  })
+
+  it('should handle two arguments ("cal arg arg" = requested month / requested year)', () => {
+    // const args = [20 , 30]
+    // deepEqual(parseArgs(args), expected)
+  })
+})
